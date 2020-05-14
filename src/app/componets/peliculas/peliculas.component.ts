ace097dac14c0cc76ecaed66b5e5d8e730163472
Anderson Jimenez <andersonmurdog@gmail.com>
2020-05-14 01:15:34 -0500

    directivas de estilo para los años

--- src/app/componets/peliculas/peliculas.component.ts
+++ src/app/componets/peliculas/peliculas.component.ts
@@ -15,10 +15,10 @@ export class PeliculasComponent implements OnInit, DoCheck, OnDestroy {
     // el constructor solo inicializa ptropiedades de clase  No se mete logica
     console.log('Constructor de hook lanzado');
     this.peliculas =  [
-      {title: 'Spiderman 4', image: 'https://media.gettyimages.com/photos/interactive-spiderman-2-attraction-is-unveiled-at-madame-tussauds-on-picture-id51071273?s=612x612'},
-      {title: 'Los vengadores', image: 'https://dam.smashmexico.com.mx/wp-content/uploads/2018/04/24160210/lo-que-debes-recordar-avengers-infinity-war.jpg'},
-      {title: 'Batman vs Superman', image: 'https://ichef.bbci.co.uk/news/ws/410/amz/worldservice/live/assets/images/2016/03/23/160323133140_batman_vs_superman_304x171_dcmovies_nocredit.jpg'},
-      {title: 'Ironman', image: 'https://www.cinemascomics.com/wp-content/uploads/2019/07/iron-man-no-muerto.jpg'},
+      {year:2020,title: 'Spiderman 4', image: 'https://media.gettyimages.com/photos/interactive-spiderman-2-attraction-is-unveiled-at-madame-tussauds-on-picture-id51071273?s=612x612'},
+      {year:2012,title: 'Los vengadores', image: 'https://dam.smashmexico.com.mx/wp-content/uploads/2018/04/24160210/lo-que-debes-recordar-avengers-infinity-war.jpg'},
+      {year:2018,title: 'Batman vs Superman', image: 'https://ichef.bbci.co.uk/news/ws/410/amz/worldservice/live/assets/images/2016/03/23/160323133140_batman_vs_superman_304x171_dcmovies_nocredit.jpg'},
+      {year:2008,title: 'Ironman', image: 'https://www.cinemascomics.com/wp-content/uploads/2019/07/iron-man-no-muerto.jpg'},
     ];
     
   }
